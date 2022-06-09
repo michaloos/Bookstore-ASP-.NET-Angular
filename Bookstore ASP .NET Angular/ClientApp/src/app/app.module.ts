784b32ea3473558032d863ca4640179b1f213789
michaloos <misialek1998@gmail.com>
2022-06-09 11:28:32 +0200

    dodane dwa komponenty (books, autors)

--- Bookstore ASP .NET Angular/ClientApp/src/app/app.module.ts
+++ Bookstore ASP .NET Angular/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BooksComponent } from './books/books.component';
+import { AutorsComponent } from './autors/autors.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    BooksComponent,
+    AutorsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +30,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'books', component: BooksComponent},
+      { path: 'autors', component: AutorsComponent},
     ])
   ],
   providers: [],
