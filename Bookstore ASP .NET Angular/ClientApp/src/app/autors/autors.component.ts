d7c371b72716af8760da8ac4a20606e7c02ca9bb
michaloos <misialek1998@gmail.com>
2022-06-10 19:29:35 +0200

    all operations, sorting

--- Bookstore ASP .NET Angular/ClientApp/src/app/autors/autors.component.ts
+++ Bookstore ASP .NET Angular/ClientApp/src/app/autors/autors.component.ts
@@ -1,15 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthorService } from '../author.service';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+import { BookService } from '../book.service';
+import { DetailAutorComponent } from '../detail-autor/detail-autor.component';
+import { DeleteAutorComponent } from '../delete-autor/delete-autor.component';
+import { AddAutorComponent } from '../add-autor/add-autor.component';
+import { EditAutorComponent } from '../edit-autor/edit-autor.component';
 
 @Component({
   selector: 'app-autors',
   templateUrl: './autors.component.html',
   styleUrls: ['./autors.component.css']
 })
-export class AutorsComponent implements OnInit {
+export class AutorsComponent implements OnInit{
+  public authors: Authors[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private matDialog: MatDialog, public bookService: BookService, public authorService: AuthorService) {
+    this.getAllAuthors();
   }
 
+  ngOnInit(){
+    this.getAllAuthors();
+  }
+
+  getAllAuthors(){
+    this.authorService.getAuthors().subscribe(result => {
+      this.authors = result;
+    }, error => console.error(error));
+  }
+
+  openDialogDelete(data){
+    var authorId = data.autorId;
+    var books: any;
+    this.bookService.getBooks().subscribe(result => {
+      books = result.filter(a=>a.autorId == authorId);
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.data = {authorData: data, booksData: books};
+      const dialogRef = this.matDialog.open(DeleteAutorComponent, dialogConfig);
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if(result == true){
+          this.authorService.deleteAuthor(authorId).subscribe(()=> {
+            this.getAllAuthors();
+          }, error => console.error(error));
+        }
+      });
+    });
+  }
+
+  openDialogDetails(data){
+    var authorId = data.autorId;
+    var books: any;
+    this.bookService.getBooks().subscribe(result => {
+      books = result.filter(a=>a.autorId == authorId);
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.data = {authorData: data, booksData: books};
+      const dialogRef = this.matDialog.open(DetailAutorComponent, dialogConfig);
+  
+      dialogRef.afterClosed();
+    });
+    
+  }
+
+  openDialogEdit(data){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = data;
+    const dialogRef = this.matDialog.open(EditAutorComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(author => {
+      if(author != null){
+        this.authorService.updateAuthor(data.autorId,author).subscribe(()=>{
+          this.getAllAuthors();
+        }, error => console.error(error));
+      }
+    });
+  }
+
+  openDialogAddAuthor(){
+    const dialogConfig = new MatDialogConfig();
+    const dialogRef = this.matDialog.open(AddAutorComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(author => {
+      if(author != null){
+        this.authorService.createAuthor(author).subscribe(()=>{
+          this.getAllAuthors();
+        }, error => console.error(error));
+      }
+    });
+  }
+
+  sortAZFirstName(){
+    this.authors = this.authors.sort((a,b) => a.fisrtName.localeCompare(b.fisrtName));
+  }
+
+  sortZAFirstName(){
+    this.authors = this.authors.sort((a,b) => b.fisrtName.localeCompare(a.fisrtName));
+  }
+
+  sortAZLastName(){
+    this.authors = this.authors.sort((a,b) => a.lastName.localeCompare(b.lastName));
+  }
+
+  sortZALastName(){
+    this.authors = this.authors.sort((a,b) => b.lastName.localeCompare(a.lastName));
+  }
+
+  cancelActions(){
+    this.getAllAuthors();
+  }
+
+  filter(data: string){
+    //this.dataSource.filter = data.trim().toLowerCase();
+    
+  }
+
+}
+
+export interface Authors {
+  autorId: number;
+  fisrtName: string;
+  lastName: string;
+  country: string;
 }
--- Bookstore ASP .NET Angular/ClientApp/src/app/books/books.component.ts
+++ Bookstore ASP .NET Angular/ClientApp/src/app/books/books.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+import { AddBookComponent } from '../add-book/add-book.component';
+import { DeleteBookComponent } from '../delete-book/delete-book.component';
+import { BookService } from '../book.service';
+import { AuthorService } from '../author.service';
+import { DetailBookComponent } from '../detail-book/detail-book.component';
+import { EditBookComponent } from '../edit-book/edit-book.component';
+import { Pipe, PipeTransform } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +15,110 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  public books: Books[];
+  public dataSource: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private matDialog: MatDialog, public bookService: BookService, public authorService: AuthorService) {
+    this.getAllBooks();
   }
 
+  getAllBooks(){
+    this.bookService.getBooks().subscribe(result => {
+      this.books = result;
+      this.dataSource = new MatTableDataSource(this.books); 
+    }, error => console.error(error));
+  }
+
+  ngOnInit(){
+    this.getAllBooks();
+  }
+
+  openDialogDelete(data) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = data;
+    var bookId = data.bookId;
+    const dialogRef = this.matDialog.open(DeleteBookComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result == true){
+        this.bookService.deleteBook(bookId).subscribe(()=> {
+          this.getAllBooks();
+        }, error => console.error(error));
+      }
+    });
+  }
+
+  openDialogAddBook(){
+    const dialogConfig = new MatDialogConfig();
+    const dialogRef = this.matDialog.open(AddBookComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(book => {
+      if(book != null){
+        this.bookService.createBook(book).subscribe(()=>{
+          this.getAllBooks();
+        }, error => console.error(error));
+      }
+    });
+  }
+
+  openDialogDetails(data){
+    var authorName: any;
+    this.authorService.getAuthor(data.autorId).subscribe(result =>{
+      authorName = result;
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.data = {bookData: data, authorData: authorName};
+      const dialogRef = this.matDialog.open(DetailBookComponent, dialogConfig);
+  
+      dialogRef.afterClosed();
+    });
+    
+  }
+
+  openDialogEdit(data){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = data;
+    var bookId = data.bookId;
+    const dialogRef = this.matDialog.open(EditBookComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(book => {
+      if(book != null){
+        this.bookService.updateBook(bookId,book).subscribe(()=>{
+          this.getAllBooks();
+        }, error => console.error(error));
+      }
+    });
+  }
+
+  sortAZ(){
+    this.books = this.books.sort((a,b) => a.title.localeCompare(b.title));
+  }
+
+  sortZA(){
+    this.books = this.books.sort((a,b) => b.title.localeCompare(a.title));
+  }
+
+  sortDateUp(){
+    this.books = this.books.sort((a,b) => a.year_of_publish - b.year_of_publish);
+  }
+
+  sortDateDown(){
+    this.books = this.books.sort((a,b) => b.year_of_publish - a.year_of_publish);
+  }
+
+  cancelActions(){
+    this.getAllBooks();
+  }
+
+  filter(data: string){
+    this.dataSource.filter = data.trim().toLowerCase();
+    
+  }
+}
+
+export interface Books {
+  bookId: number;
+  title: string;
+  autorId: number;
+  year_of_publish: number;
+  ISBN: string;
 }
